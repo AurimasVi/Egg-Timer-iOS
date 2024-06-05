e92928f9079c3f89a94247e7d19c0d4937869e41
AurimasVi <aurimas.vidutis@gmail.com>
2024-06-05 22:55:00 +0300

    Added console timer countdown

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,41 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
+   
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    
+    var secondsRemaining: Int = 0
+
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        func startTimer(seconds: Int) {
+            secondsRemaining = seconds
+            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self] (Timer) in
+                   if secondsRemaining > 0 {
+                       print ("\(secondsRemaining) seconds")
+                       secondsRemaining -= 1
+                   } else {
+                       Timer.invalidate()
+                   }
+               }
+           }
+        if hardness == "Soft" {
+            startTimer(seconds: 300)
+        } else if hardness == "Medium" {
+            startTimer(seconds: 420)
+        } else if hardness == "Hard" {
+            startTimer(seconds: 720)
+        }
+        
+    }
     
 
-
 }
+
+
+    
+
